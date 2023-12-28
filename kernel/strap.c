70bca4c6575109ee6cf3773d2386a458ba41551e
minicago <vipbyy@126.com>
2023-12-28 16:12:49 +0800

    my work on lab2_c1 is changed.

--- kernel/strap.c
+++ kernel/strap.c
@@ -62,10 +62,15 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
       // panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
-    if(stval >= current->trapframe->regs.sp )
       
-     user_vm_map((pagetable_t)current->pagetable, stval, 1, (uint64)alloc_page(),
-            prot_to_type(PROT_WRITE | PROT_READ, 1));
+      if(stval >= current->trapframe->regs.sp && stval < current->trapframe->regs.sp + PGSIZE)
+      {
+        user_vm_map((pagetable_t)current->pagetable, stval, 1, (uint64)alloc_page(),
+              prot_to_type(PROT_WRITE | PROT_READ, 1));
+      }  
+      else{
+        panic("this address is not available!");
+      }
       
       break;
     default:
